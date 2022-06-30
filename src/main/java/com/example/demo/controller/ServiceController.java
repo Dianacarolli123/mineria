24a81bfca1c5350724e93b51cdf6aef900b08576
Dianacarolli123 <dianacarolli@hotmail.com>
2022-06-30 14:05:45 -0500

    prueba de cambio profe

--- src/main/java/com/example/demo/controller/ServiceController.java
+++ src/main/java/com/example/demo/controller/ServiceController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServiceController {
     @GetMapping("/")
     public String index() {
-        return "Buenas noches";
+        return "Buenas noches y hola como está";
     }
 }
